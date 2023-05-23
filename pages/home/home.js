8bc68c7efa35fc58c6b531f960b497fd031a55fe
Selenium39 <selenium39@qq.com>
2023-05-23 22:44:49 +0800

    home

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,7 +1,41 @@
 // index.js
+const app = getApp()
 Page({
   data: {
     active: 'home',
+    token: null,
+    searchValue:'',
+    userInfo:{},
+  },
+  gotoLogin() {
+    wx.navigateTo({
+      url: '/pages/login/login',
+    })
+  },
+  showRepoList(){
+    const data = {
+      token:this.data.token,
+      owner:this.data.userInfo.login
+    }
+    wx.navigateTo({
+      url: `/pages/repo-list/repo-list?data=${encodeURIComponent(JSON.stringify(data))}`,
+    })
+  },
+  showStarredList(){
+    const data = {
+      token:this.data.token,
+    }
+    wx.navigateTo({
+      url: `/pages/starred/starred?data=${encodeURIComponent(JSON.stringify(data))}`,
+    })
+  },
+  showOrganList(){
+    const data = {
+      token:this.data.token,
+    }
+    wx.navigateTo({
+      url: `/pages/organs/organs?data=${encodeURIComponent(JSON.stringify(data))}`,
+    })
   },
   onChange(event) {
     // event.detail 的值为当前选中项的索引
@@ -12,5 +46,12 @@ Page({
   },
   onShow: function () {
     this.getTabBar().init();
+    if (app.globalData.token) {
+      const token = app.globalData.token
+      this.setData({
+        token,
+        userInfo:app.globalData.userInfo
+      })
+    }
   },
 });
