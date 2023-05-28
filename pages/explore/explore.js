97e879e505584650e873293443bd7e8c5d5fa812
Selenium39 <selenium39@qq.com>
2023-05-28 23:50:55 +0800

    explore page add search

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -1,5 +1,23 @@
 Page({
+  data:{
+    searchValue:''
+  },
   onShow: function () {
     this.getTabBar().init();
+  },
+  onSearch(){
+    if(this.data.searchValue){
+      const data = {
+        searchValue:this.data.searchValue
+      }
+      wx.navigateTo({
+        url: `/pages/search/search?data=${encodeURIComponent(JSON.stringify(data))}`,
+      })
+    }
+  },
+  searchChange(event){
+    this.setData({
+      searchValue: event.detail,
+    });
   },
 })
