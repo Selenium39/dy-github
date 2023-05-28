5b259e6a4f35c3582272438efa80c555c357cda9
Selenium39 <selenium39@qq.com>
2023-05-28 20:15:17 +0800

    brower code using markdown

--- pages/repo-code/repo-code.js
+++ pages/repo-code/repo-code.js
@@ -4,17 +4,20 @@ const app = getApp()
 
 Page({
   data: {
-    fileContent:null,
+    fileContent: null,
   },
   async onLoad(options) {
     const { owner, repo, path } = JSON.parse(decodeURIComponent(options.data))
-    const {content} = await repoService.getRepoContent({
+    const { content } = await repoService.getRepoContent({
       owner,
       repo,
       path
     })
-    const contentStr = base64Util.decode(content)
-    const fileContent = app.towxml(contentStr,'markdown')
+    const matches = path.match(/\.([^./\\]+)$/)
+    const language = matches ? matches[1] : 'shell'
+    console.log(language)
+    const contentStr = '```' + language + base64Util.decode(content) + '```'
+    const fileContent = app.towxml(contentStr, 'markdown')
     this.setData({
       fileContent
     })
